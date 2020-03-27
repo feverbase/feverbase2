d1daca58df84ef34f4e463da02a21d402aafe122
Noah Saso <noahsaso@gmail.com>
2020-03-27 11:55:35 -0700

    add summary fields to clinicaltrials.gov output

--- sources/clinicaltrialsgov.py
+++ sources/clinicaltrialsgov.py
@@ -3,9 +3,23 @@ import os
 import utils
 from dateutil import parser
 from datetime import timezone
+from bs4 import BeautifulSoup
 
 FILENAME = "clinicaltrialsgov.json"
 POSTED_WITHIN_DAYS = 200  # posted within the last X days
+
+def summary_to_dict(summary):
+    fields = {}
+    soup = BeautifulSoup(summary, "html.parser")
+    for b in soup.findAll('b'):
+        key = b.get_text()
+                                            # remove colon
+        value = b.next_sibling and b.next_sibling[1:].replace(u'\xa0', u' ').strip()
+        if 'recruit' in key.lower() and not value:
+            value = key
+            key = 'Stage'
+        fields[key] = value
+    return fields
 
 def find(term):
     data = {}
@@ -38,7 +52,6 @@ def find(term):
 
         title = entry["title"]
         url = entry["link"].replace(f'cond={term}&', '')
-        summary = entry["summary"]
 
         # published = entry['published_parsed']
         # iso = time.strftime('%Y-%m-%dT%H:%M:%S%z', published)
@@ -47,13 +60,22 @@ def find(term):
         )  # '%a, %d %b %Y %H:%M:%S %Z'
         iso = date.astimezone(timezone.utc).isoformat()
 
+        # fields = summary_to_dict(entry["summary"])
+
         entry_dict = {
             "title": title,
             "url": url,
             "timestamp": iso,
+            # "fields": fields,
         }
+
+        for key, value in summary_to_dict(entry["summary"]).items():
+            entry_dict[key] = value
+
         data[url] = entry_dict
 
     print(f"Fetched {len(data)} results")
 
     return data
+
+print(find('coronavirus'))
