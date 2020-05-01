cb9d7d2a24050ef7f556817fc32450a8b1ec178d
Ethan Houston <ethan.houston@gmail.com>
2020-05-01 09:25:10 -0700

    Add article parsing method

--- location/__init__.py
+++ location/__init__.py
@@ -13,50 +13,104 @@ load_dotenv()
 
 if os.environ.get("GOOGLE_MAPS_KEY"):
     key = os.environ.get("GOOGLE_MAPS_KEY")
-    BASE_URL = f"https://mapas.googleapis.com/maps/api/geocode/json?key={key}"
+    BASE_URL = f"https://maps.googleapis.com/maps/api/geocode/json?key={key}"
 
 
-def get_location(query):
-    # first look it up in mongo
-    mongo_results = db.Location.objects(institution=query)
-    if len(mongo_results) > 0:
-        res = mongo_results[0]
-        location = res["location"]
+"""
+run from fetch/__init__.py, take the entire list of
+articles as input, and output the same list with each
+article's location data
+"""
+def add_location_data(articles):
+    institutions = [a.get("institution") for a in articles]
 
-        return {
-            "address": location["address"],
-            "latitude": location["latitude"],
-            "longitude": location["longitude"],
-        }
-    # if it doesn't exist in mongo, add it then return
-    else:
-        url = BASE_URL + f"&address={query}" 
-        data = requests.get(url).json().get("results")
+    # geocode all locations (this only peforms the
+    # operation if the location is not already in the
+    # database.)
+    new_locations = [l for l in get_locations(institutions) if l]
 
-        if data:
-            # always just take the first item for now
-            if len(data) >= 1:
-                result = data[0]
-                geometry = result.get("geometry", None)
-                if geometry:
-                    location = geometry.get("location", None)
-                    if location:
-                        lat = location.get("lat")
-                        lng = location.get("lng")
-                address = result.get("formatted_address", None)
+    # push all these newly-fetched locations to the
+    # database
+    if len(new_locations) > 0:
+        batch_insert_locations(new_locations)
 
-                location_details = {
-                    "address": address,
-                    "latitude": lat,
-                    "longitude": lng,
-                }
-                location = {"institution": query, "location": location_details}
-                obj = db.Location(**location)
-                db.Location.smart_insert([obj])
+    # get all mongo ids so we have a mapping from
+    # institution -> location_id, including the
+    # new ones we just pushed
+    location_mappings = get_mongo_ids()
 
-                location = obj["location"]
-                return {
-                    "address": location["address"],
-                    "latitude": location["latitude"],
-                    "longitude": location["longitude"],
-                }
+    # for every article, lookup related institution
+    # in our mapping, and add the object_id relating
+    # to its location data
+    for article in articles:
+        institution = article.get("institution", None)
+        if institution:
+            location_id = location_mappings.get(institution)
+            if location_id:
+                article["location_data"] = location_id
+        else:
+            article["location_data"] = None
+
+    return articles
+
+"""
+run this first on every article, return a list of
+all 'new' locations. to be bulk inserted into mongo
+"""
+def get_locations(queries):
+    location_data = []
+    for query in queries:
+        # if location is already in the db, skip
+        if not location_in_db(query):
+            this_location_data = geocode_query(query)
+            location_data.append(this_location_data)
+    return location_data
+
+
+def location_in_db(query):
+    return len(db.Location.objects(institution=query)) > 0
+
+def get_mongo_ids():
+    # get all locations from mongo
+    all_locations = db.Location.objects()
+    # read into dictionary structure
+    mappings = {}
+    for location in all_locations:
+        mappings[location["institution"]] = location.id
+    return mappings
+
+def batch_insert_locations(locations):
+    objects = []
+    for location in locations:
+        obj = db.Location(**location)
+        objects.append(obj)
+    db.Location.smart_insert(objects)
+
+"""
+returns {"address": ..., "latitude": ..., "longitude": .. }
+"""
+def geocode_query(query):
+    url = BASE_URL + f"&address={query}"
+    data = requests.get(url).json().get("results")
+
+    if data:
+        # always just take the first item for now
+        if len(data) > 0:
+            result = data[0]
+            geometry = result.get("geometry", None)
+            if geometry:
+                location = geometry.get("location", None)
+                if location:
+                    lat = location.get("lat")
+                    lng = location.get("lng")
+            address = result.get("formatted_address", None)
+
+            location_details = {
+                "institution": query,
+                "address": address,
+                "latitude": lat,
+                "longitude": lng,
+            }
+
+            return location_details
+
